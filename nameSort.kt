6a65711c18d83a499c382290909bcdb24a507868
mariamrls7 <122957790+mariamrls7@users.noreply.github.com>
2023-02-24 21:35:55 -0600

    Update nameSort.kt

--- nameSort.kt
+++ nameSort.kt
@@ -1,10 +1,5 @@
 fun main() {
-    val people = listOf(people("Charlie",54), people("Leslie", 34),
-            people("Andy", 11))
-     
-    println("Sorting by ascending: ")
-    val sortedPeople = people.sortedBy { people -> people.name }
-    sortedPeople.forEach { p -> println(p.name) }
-}
+	val names = listOf("Charlie", "Leslie", "Andy")
 
-class people(val name: String,marks : Int)
+	println("Sorted ascending: ${names.sorted()}")
+}
