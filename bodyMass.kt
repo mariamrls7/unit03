44c5539cb255809ee330a475803ac3c5654b1ac1
mariamrls7 <122957790+mariamrls7@users.noreply.github.com>
2023-02-26 23:28:32 -0600

    Update bodyMass.kt

--- bodyMass.kt
+++ bodyMass.kt
@@ -1,13 +1,24 @@
-fun main(){
-   print('BMI Calculator')
-    height = eval(input('Enter your height in inches; '))
-    weight = eval(input('Enter your weight in pounds:))
+import java.util.Scanner
 
-    bmi = weight * 703 / height * height
-        if bmi >= 18.5 and bmi <= 25:
-            print("You have an optimal weight")
-        elif bmi < 18.5
-                print("You are underweight")
-        else bmi > 25
-                print("You are overweight")
+//BMI = Weight*703/Height^2
+
+fun main(args: Array<String>) {
+
+    val scanner  = Scanner(System.`in`)
+    val weight= scanner.nextInt()
+    println("Enter your weight in lbs: $weight")
+    val height= scanner.nextInt()
+    println("Enter your height: $height")
+    val bmi= (weight * 703f)/(height*height)
+
+    println("Your BMI: $bmi")
+if (bmi < 18.5f) {
+print("You are considered underweight")
+}
+if (bmi > 25f){
+print("You are considered overweight")
+}
+else {
+print("Your weight is optimal")
+}
 }
